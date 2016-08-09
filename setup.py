36bfc7529e7b79a5547cc7dfd018311ac51b9b21
Oz N Tiram <oz.tiram@gmail.com>
2016-08-09 17:32:13 +0200

    Restructure project
     add setup and tests

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='fernet',
+      version=0.1,
+      description='A simple python fernet implementation',
+      author='Oz N Tiram',
+      author_email='oz.tiram@gmail.com',
+      url='https://github.com/oz123/python-fernet',
+      py_modules=['fernet'],
+      license='MIT',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Security :: Cryptography',
+                   ],
+      )
